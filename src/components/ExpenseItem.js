515b9b9516d51a94afbecd36d715bb2e150954ca
jyotirmoyWork <jyotirmoy@mukherjee.cyou>
2022-03-25 02:07:51 +0530

    component divided

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,12 @@
 import "./Expense.css"
-
-function ExpenseItem() {
-
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
-
+import ExpenseDate from './ExpenseDate';
+function ExpenseItem(props) {
     return (
-        <div>
-            <div className="expense-item">
-                <div>{expenseDate.toISOString()}</div>
-                <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
-                </div>
+        <div className='expense-item'>
+              <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     )
